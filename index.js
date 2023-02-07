84bc6d147dbe2095dc4d446baa4d598ebf0fa64e
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-02-07 19:49:48 +0300

    Сделать js и переделать модальное окно

---
+++ index.js
@@ -0,0 +1,35 @@
+let content = document.querySelector('.content');
+let profileName = content.querySelector('.profile__name');
+let profileSpecialization = content.querySelector('.profile__specialization');
+let editButton = content.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+console.log(popup);
+let popupName = popup.querySelector('.popup__name');
+let popupSpecialization = popup.querySelector('.popup__specialization');
+let popupBtn = popup.querySelector('.popup__btn');
+let popupCloseBtn = popup.querySelector('.popup__close-btn');
+
+console.log(profileName.innerHTML);
+
+
+function clickEditButton(){
+  popupName.value = profileName.innerHTML;
+  popupSpecialization.value = profileSpecialization.innerHTML;
+  popup.setAttribute('style', 'display: flex');
+}
+
+function clickCloseButton(){
+  popup.setAttribute('style', 'display: none');
+}
+
+editButton.addEventListener('click', clickEditButton);
+popupCloseBtn.addEventListener('click', clickCloseButton);
+
+function clickSaveButton(){
+  profileName.textContent = popupName.value;
+  profileSpecialization.textContent = popupSpecialization.value;
+  popup.setAttribute('style', 'display: none');
+}
+
+popupBtn.addEventListener('click', clickSaveButton);
