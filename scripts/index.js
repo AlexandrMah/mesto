dad521900feb58e995c98c207878572067b14d12
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-02-11 16:14:14 +0300

    Ревью 3

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,18 @@ let profileSpecialization = content.querySelector('.profile__specialization');
 let editButton = content.querySelector('.profile__edit-button');
 
 let popup = document.querySelector('.popup');
-let popupInput = popup.querySelector('.popup__input')
-let popupName = popup.querySelector('.popup__element_key_name');
-let popupSpecialization = popup.querySelector('.popup__element_key_specialization');
+let formElement = popup.querySelector('.popup__input')
+let nameInput = popup.querySelector('.popup__element_key_name');
+let jobInput = popup.querySelector('.popup__element_key_specialization');
 let popupBtn = popup.querySelector('.popup__btn');
 let popupCloseBtn = popup.querySelector('.popup__close-btn');
 
+let nam;
+let job;
+
 function clickEditButton(){
-  popupName.value = profileName.textContent;
-  popupSpecialization.value = profileSpecialization.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileSpecialization.textContent;
   popup.classList.add('popup_opened');
 }
 
@@ -20,26 +23,15 @@ function clickCloseButton(){
   popup.classList.remove('popup_opened');
 }
 
-function serializeForm(formNode) {
-  return new FormData(formNode);
-}
+function handleFormSubmit (evt) {
+  evt.preventDefault();
 
-async function sendData(data) {
-  return await fetch('/api/apply/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'multipart/form-data' },
-    body: data,
-  })
-}
+  profileName.textContent = nameInput.value;
+  profileSpecialization.textContent = jobInput.value;
 
-async function handleFormSubmit(event){
-  event.preventDefault();
-
-  const data = serializeForm(event.target);
-  const response = await sendData(data);
   popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', clickEditButton);
 popupCloseBtn.addEventListener('click', clickCloseButton);
-popupInput.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit); 
